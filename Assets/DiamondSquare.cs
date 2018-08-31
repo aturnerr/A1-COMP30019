71cc79d6e305b6478098133ce592ba9891ad8068
amiexzj <amiexzj@gmail.com>
2018-08-31 12:24:10 +1000

    diamond square

---
+++ Assets/DiamondSquare.cs
@@ -0,0 +1,92 @@
+﻿// Code adapted from original creator Ather Omar @
+// https://www.youtube.com/watch?v=1HV8GbFnCik
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondSquare : MonoBehaviour {
+
+    // number of faces 
+    public int nFaces;
+   
+    // terrain dimensions
+    public float size;
+    public float height;
+
+    // vertex array 
+    Vector3[] vertices;
+    // vertex count
+    int nVertices;
+
+
+	// Use this for initialization
+	void Start () {
+
+        CreateTerrain();
+
+	}
+	
+	// 
+	void CreateTerrain () {
+        
+        // number of vertices in the terrain
+        nVertices = (nFaces + 1) * (nFaces + 1);
+        // array holding all the vertices
+        vertices = new Vector3[nVertices];
+        // U,V positions of the vertices
+        Vector2[] uvs = new Vector2[nVertices];
+        // triangles in the terrain 
+        int[] tris = new int[nFaces * nFaces * 6];
+
+        float halfSize = size * 0.5f;
+        float faceSize = size / nFaces;
+
+        // create new mesh 
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        int triOffset = 0;
+
+        // iterate over vertices 
+        for (int i = 0; i <= nFaces; i++)
+        {
+            for (int j = 0; i <= nFaces; j++)
+            {
+                // initialise vertices and uvs row by row
+                vertices[i * (nFaces + 1) + j] = new Vector3(-halfSize + j * faceSize, 0.0f, halfSize - i * faceSize);
+                uvs[i * (nFaces + 1) + j] = new Vector2((float) i / nFaces, (float) j / nFaces);
+
+                // check if finished
+                if (i < nFaces && j < nFaces)
+                {
+                    // access correct triangle
+                    int topLeft = i * (nFaces + 1) + j;
+                    int botLeft = (i + 1) * (nFaces + 1) + j;
+
+                    // our first triangle
+                    tris[triOffset] = topLeft;
+                    tris[triOffset + 1] = topLeft + 1;
+                    tris[triOffset + 2] = botLeft + 1;
+
+                    // second triangle
+                    tris[triOffset + 3] = topLeft;
+                    tris[triOffset + 4] = botLeft + 1;
+                    tris[triOffset + 5] = botLeft;
+
+                    triOffset += 6;
+                }
+
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uvs;
+        mesh.triangles = tris;
+
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+	}
+}
